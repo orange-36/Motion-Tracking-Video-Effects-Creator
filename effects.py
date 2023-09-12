a4dc974badbbca113184af769a6a424917b68a2c
orange-36 <afour505@gmail.com>
2023-09-13 00:04:38 +0800

    Added: Rainbow Halo Effect Feature

--- effects.py
+++ effects.py
@@ -5,7 +5,7 @@ import numpy as np
 from collections import Counter
 from torchvision.transforms.functional import rotate, adjust_brightness, adjust_contrast, affine
 from torchvision.transforms import Grayscale
-from utils.utils import get_lens_flare, show_img
+from utils.utils import get_lens_flare, show_img, rotate_point, rainbow_colors
 
 class BasicEffect:
     def __init__(self) -> None:
@@ -49,18 +49,16 @@ class AfterimageEffect(BasicEffect):
         return org_frame
     
 class LightTrackEffect(BasicEffect):
-    def __init__(self, afterimage_interval_ratio=0.33, residual_time=0.2, alpha_start=0.1, alpha_end=1, gradient=True, rainbow_round_time=-1, color=[255, 255, 255]) -> None:
+    def __init__(self, afterimage_interval_ratio=0.33, residual_time=0.2, alpha_start=0.1, alpha_end=1, gradient=True, rainbow_repeat_interval=-1, color=[255, 255, 255], rainbow_colors=rainbow_colors) -> None:
         super().__init__()
         self.afterimage_interval_ratio = afterimage_interval_ratio
         self.residual_time = residual_time
         self.alpha_start = alpha_start
         self.alpha_end = alpha_end
         self.gradient = gradient    
-        self.rainbow_round_time = rainbow_round_time    
+        self.rainbow_repeat_interval = rainbow_repeat_interval    
         self.color = color          
-        self.rainbow_colors = torch.tensor([[0, 0, 255], [0, 67, 255], [0, 135, 255], [0, 173, 255], [0, 211, 255], [5, 233, 238], [10, 255, 222], [10, 255, 191], [10, 255, 161], [81, 255, 85], [153, 255, 10], 
-                                        [204, 247, 10], [255, 239, 10], [250, 182, 15], [245, 125, 20], [250, 67, 54], [255, 10, 88], [255, 10, 139], [255, 10, 190], [127, 5, 222]], 
-                                        dtype=torch.uint8) # BGR Red to Rurple
+        self.rainbow_colors = torch.tensor(rainbow_colors,  dtype=torch.uint8)
         
     def perform_editing(self, org_frame: torch.tensor, frame_idx: int, mask_memory_frames: Deque[torch.BoolTensor], object_memory_frames: Deque[torch.tensor], **kwargs):
         frame_interval = int(self.residual_time*self.fps*self.afterimage_interval_ratio)
@@ -71,12 +69,12 @@ class LightTrackEffect(BasicEffect):
             return org_frame
         alpha_for_object_list = np.linspace(self.alpha_start, self.alpha_end, len(from_current_list), endpoint=False)
 
-        if self.rainbow_round_time>0:
-            aver_rainbow_color_change_frame = self.fps*self.rainbow_round_time/len(self.rainbow_colors)
+        if self.rainbow_repeat_interval>0:
+            aver_rainbow_color_change_frame = self.fps*self.rainbow_repeat_interval/len(self.rainbow_colors)
             rainbow_colors_num = len(self.rainbow_colors)
 
         for from_current, alpha_for_object in zip(from_current_list, alpha_for_object_list):
-            if self.rainbow_round_time>0:
+            if self.rainbow_repeat_interval>0:
                 color_track = self.rainbow_colors[int((frame_idx-from_current)//aver_rainbow_color_change_frame)%rainbow_colors_num].to(self.device)
             else:
                 color_track = torch.tensor(self.color, dtype=torch.uint8).to(self.device)
@@ -104,9 +102,15 @@ class KaleidoscopeEffect(BasicEffect):
         # show_img(object_img, figsize=(5, 5))
         object_img = object_img.permute(2, 0, 1)
         angle = self.angle
+        if len(object_centroids)>0 and len(object_centroids[-1]):
+            centroids_list = object_centroids[-1]
+            (x, y) = centroids_list[0]
         for degree in range(angle, 361, angle):
             copy = rotate(object_img, angle=degree, center=self.center)
             object_img = torch.bitwise_or(object_img, copy)
+            if len(object_centroids)>0 and len(object_centroids[-1]):
+                object_centroids[-1].append(rotate_point(x, y, self.center[0], self.center[1], degree))
+
             angle = angle*2
         object_img = object_img.permute(1, 2, 0)
         # show_img(copy.permute(1, 2, 0), figsize=(5, 5))
@@ -142,23 +146,29 @@ class AdjustBrightnessAndContrast(BasicEffect):
         return org_frame
 
 class LensFlareEffect(BasicEffect):
-    def __init__(self, radius=50, test = False, center_brightness=10, apply_entire_track=True, rendering_effect=False, afterimage_interval_ratio=0.33, residual_time=0.2, alpha_start=0.1, alpha_end=1) -> None:
+    def __init__(self, radius=50, test = False, center_brightness=10, apply_entire_track=True, rendering_effect=False, afterimage_interval_ratio=0.33, 
+                 residual_time=0.2, alpha=0.1, alpha_decay_rated=0.7, bgr_weights=[1, 1, 1], rainbow_repeat_interval=-1, rainbow_colors=rainbow_colors) -> None:
         super().__init__()
         self.radius = radius
-        self.lens_flare = (center_brightness*get_lens_flare(radius=self.radius)).type(dtype=torch.uint8).to(self.device).view(2*radius+1, 2*radius+1, 1)
+        self.lens_flare = (center_brightness*get_lens_flare(radius=self.radius)).to(self.device)
+        self.bgr_weights =torch.tensor(bgr_weights).to(self.device)
 
         self.afterimage_interval_ratio = afterimage_interval_ratio
         self.residual_time = residual_time
         self.apply_entire_track = apply_entire_track        
         self.rendering_effect = rendering_effect
-        self.alpha_start = alpha_start
-        self.alpha_end = alpha_end
+        self.alpha = alpha
+        self.alpha_decay_rated = alpha_decay_rated
         self.test = test
+
+        self.rainbow_repeat_interval = rainbow_repeat_interval
+        self.rainbow_colors = (torch.tensor(rainbow_colors)/255).to(self.device)
+        
 
     def config_setting(self) -> Dict[str, int]:
         return {"use_object_centroids": True}
 
-    def perform_editing(self, org_frame: torch.tensor, object_centroids: Deque[Tuple[int, int]], **kwargs):
+    def perform_editing(self, org_frame: torch.tensor, frame_idx: int, object_centroids: Deque[Tuple[int, int]], **kwargs):
         frame_interval = math.ceil(self.residual_time*self.fps*self.afterimage_interval_ratio)
         # if frame_interval < 1:
         #     print(frame_interval)
@@ -166,31 +176,50 @@ class LensFlareEffect(BasicEffect):
             from_current_list = range(1, min(len(object_centroids), int(self.residual_time*self.fps)), frame_interval)[::-1]
             if len(from_current_list)==0:
                 return org_frame
-            alpha_for_object_list = np.linspace(self.alpha_start, self.alpha_end, len(from_current_list), endpoint=True)
+            # alpha_for_object_list = np.linspace(self.alpha_start, self.alpha_end, len(from_current_list), endpoint=True)
+            alpha_for_object_list = [1]
+            for i in range(1, len(from_current_list)):
+                alpha_for_object_list.append(self.alpha_decay_rated* alpha_for_object_list[-1])
+            alpha_for_object_list = alpha_for_object_list[::-1]
         else:
             from_current_list = [1]
             alpha_for_object_list = [1]
 
-        all_lens_flare = torch.zeros((org_frame.shape[0], org_frame.shape[1], 1), dtype=torch.uint8).to(self.device)
+        if self.rainbow_repeat_interval>0:
+            rainbow_colors_num = len(rainbow_colors)
+            aver_rainbow_color_change_frame = self.fps*self.rainbow_repeat_interval/rainbow_colors_num
+        all_lens_flare = torch.zeros((org_frame.shape[0], org_frame.shape[1], 3), dtype=torch.uint8).to(self.device)
         for from_current, alpha_for_object in zip(from_current_list, alpha_for_object_list):
             # print(org_frame)
             centroids_in_one_frame = object_centroids[-from_current]
             # print(f"from_current: {from_current} alpha_for_object {alpha_for_object}")
             for (center_x, center_y) in centroids_in_one_frame:
-                one_lens_flare = torch.zeros((org_frame.shape[0], org_frame.shape[1], 1)).to(self.device)
-                upper = np.ceil(min(center_y, self.radius))
-                lower = np.floor(min(org_frame.shape[0]-center_y, self.radius))
-                left  = np.ceil(min(center_x, self.radius))
-                right = np.floor(min(org_frame.shape[1]-center_x, self.radius))
-                one_lens_flare[int(center_y-upper): int(center_y+lower), int(center_x-left): int(center_x+right)] = self.lens_flare[int(self.radius-upper): int(self.radius+lower), int(self.radius-left): int(self.radius+right)] 
+                one_lens_flare = torch.zeros((org_frame.shape[0], org_frame.shape[1], 3)).to(self.device)
+                center_x, center_y = int(center_x), int(center_y)
+                upper = int(np.floor(max(center_y-self.radius, 0)))
+                lower = int(np.ceil(min(center_y+self.radius, org_frame.shape[0])))
+                left  = int(np.floor(max(center_x-self.radius, 0)))
+                right = int(np.ceil(min(center_x+self.radius, org_frame.shape[1])))
+                if(lower<0 or upper>org_frame.shape[0] or right<0 or left>org_frame.shape[1]):
+                    continue
+                pattern_upper = self.radius + upper - center_y
+                pattern_lower = self.radius + lower - center_y
+                pattern_left = self.radius + left - center_x
+                pattern_right = self.radius + right - center_x
+
+                if self.rainbow_repeat_interval>0:
+                    bgr_weights = self.rainbow_colors[int((frame_idx-from_current)//aver_rainbow_color_change_frame)%rainbow_colors_num].to(self.device)
+                else:
+                    bgr_weights = self.bgr_weights.to(self.device)
+                color_lens_flare = bgr_weights* self.lens_flare.to(self.device)
+                one_lens_flare[upper: lower, left: right] = color_lens_flare[pattern_upper: pattern_lower, pattern_left: pattern_right] 
                 one_lens_flare = (alpha_for_object*one_lens_flare).type(torch.uint8)
+
                 if self.rendering_effect:
                     all_lens_flare = torch.bitwise_or(input=all_lens_flare, other=one_lens_flare)
                 else:
                     all_lens_flare = torch.max(one_lens_flare, all_lens_flare)
 
-                # show_img(all_lens_flare.cpu())
-                # print(Counter(all_lens_flare.flatten().tolist()))
         org_frame = org_frame + all_lens_flare.type(torch.float16)
         org_frame = torch.clamp(org_frame, min=0, max=255).type(torch.uint8)
         
--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,12 @@ from matplotlib import pyplot as plt
 import numpy as np
 import torch
 
+
+
+ # BGR Red to Rurple
+rainbow_colors = [[0, 0, 255], [0, 67, 255], [0, 135, 255], [0, 173, 255], [0, 211, 255], [5, 233, 238], [10, 255, 222], [10, 255, 191], [10, 255, 161], [81, 255, 85], [153, 255, 10], 
+                                        [204, 247, 10], [255, 239, 10], [250, 182, 15], [245, 125, 20], [250, 67, 54], [255, 10, 88], [255, 10, 139], [255, 10, 190], [127, 5, 222]]
+
 def show_img(img, figsize=(5, 5)):
     plt.figure(figsize=figsize)
     if img.shape[-1]==3:
@@ -10,7 +16,7 @@ def show_img(img, figsize=(5, 5)):
         plt.imshow(img, cmap='gray')
     plt.show()
 
-def get_lens_flare(radius=5):
+def get_lens_flare(radius=5, rgb_weights=[1, 1, 1]):
     rows, cols = 1+2*radius, 1+2*radius
     flare_effect = torch.zeros(size=(rows, cols))
     for i in range(rows):
@@ -19,5 +25,21 @@ def get_lens_flare(radius=5):
             if(distance<=radius):
                 flare_effect[i][j] = radius-distance
     # all value in range [0, 1]
+    flare_effect = flare_effect.view(rows, cols, 1)
     flare_effect = flare_effect/radius
+    # rgb_weights = torch.tensor(rgb_weights)
     return flare_effect
+
+def rotate_point(x, y, cx, cy, angle_degrees):
+    angle_radians = np.radians(angle_degrees)
+
+    rotation_matrix = np.array([[np.cos(angle_radians), -np.sin(angle_radians)],
+                                 [np.sin(angle_radians), np.cos(angle_radians)]])
+
+    point_vector = np.array([x - cx, y - cy])
+
+    rotated_vector = np.dot(rotation_matrix, point_vector)
+    x_new = rotated_vector[0] + cx
+    y_new = rotated_vector[1] + cy
+
+    return x_new, y_new
